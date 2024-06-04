d4fa3e55284790a21898ca98cd1e56bf73a20728
Resul Elezi <resul_elezi@protonmail.com>
2024-06-04 22:07:01 +0200

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,6 +137,7 @@ const products = [
     (btn) => {
       btn.addEventListener("click", (event) => {
       cart.addItem(Number(event.target.id), products);
+      totalNumberOfItems.textContent = cart.getCounts();
       })
     }
   );
