30f064b27f01409b56fe59f8c011c46ea7eddaba
Resul Elezi <resul_elezi@protonmail.com>
2024-06-05 21:24:34 +0200

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,8 +134,14 @@ const products = [
           alert("Your shopping cart is already empty");
           return;
         }
+        const isCartCleared = confirm(
+          "Are you sure you want to clear all items from your shopping cart?"
+        );
+        if (isCartCleared) {
+          this.items = [];
+          this.total = 0;
     
-    
+        }
       }
       calculateTaxes(amount) {
         return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
